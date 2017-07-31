1e1ccd19a09b454a02b2c18ffe61ac45cc116708
kic <kic@kic.pl>
2017-07-31 19:37:02 +0200

    update dean office main, urls, views

--- App/deanOffice/urls.py
+++ App/deanOffice/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_site, name='main_site'),
+    url(r'^deanOffice$', views.dean_office, name='dean_office'),
+    url(r'^deanOffice/nextNumber$', views.dean_next_number, name='dean_next_number'),
+    url(r'^welfareOffice$', views.welfare_office, name='welfare_office'),
+    url(r'^welfareOffice/nextNumber$', views.welfare_next_number, name='welfare_next_number'),
 ]
--- App/deanOffice/views.py
+++ App/deanOffice/views.py
@@ -3,7 +3,7 @@ from .models import Student
 from .models import Employee
 from .models import NumbersQueue
 # Create your views here.
-def main_site(request):
+def dean_office(request):
     current_user = Employee.objects.first()
 
     num = NumbersQueue.objects.first()
@@ -21,4 +21,34 @@ def main_site(request):
         student = num.studentId
 
 
-    return render(request, 'dean_office/main.html', {'num': num, 'student': student, 'current_user': current_user})
+    return render(request, 'dean_office/main.html', {'num': num, 'student': student, 'current_user': current_user, 'nextNumberLink': 'deanOffice/nextNumber'})
+
+
+def dean_next_number(request):
+    NumbersQueue.objects.first().delete()
+    return dean_office(request)
+
+def welfare_office(request):
+    current_user = Employee.objects.first()
+
+    num = NumbersQueue.objects.first()
+
+    if num is None:
+        num = NumbersQueue()
+        num.numberId = 0
+        student = Student()
+        student.studentId = "---"
+        student.firstName = "---"
+        student.lastName = ""
+        student.faculty = "---"
+        student.groupId = "---"
+    else:
+        student = num.studentId
+
+
+    return render(request, 'dean_office/main.html', {'num': num, 'student': student, 'current_user': current_user, 'nextNumberLink': 'welfareOffice/nextNumber'})
+
+
+def welfare_next_number(request):
+    NumbersQueue.objects.first().delete()
+    return welfare_office(request)
