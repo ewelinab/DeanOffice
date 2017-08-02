df08560a89bd9c6766a13c66f16e3fbf216967ac
kic <kic@kic.pl>
2017-08-03 00:11:32 +0200

    Add two services - GetActualNumber and GetAvailableNumber

--- App/deanOffice/urls.py
+++ App/deanOffice/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^deanOffice$', views.dean_office, name='dean_office'),
     url(r'^deanOffice/nextNumber$', views.dean_next_number, name='dean_next_number'),
+    url(r'^deanOffice/getActualNumber$', views.dean_get_actual_number, name='dean_get_actual_number'),
+    url(r'^deanOffice/getAvailableNumber$', views.dean_get_available_number, name='dean_get_available_number'),
     url(r'^welfareOffice$', views.welfare_office, name='welfare_office'),
     url(r'^welfareOffice/nextNumber$', views.welfare_next_number, name='welfare_next_number'),
+
 ]
--- App/deanOffice/views.py
+++ App/deanOffice/views.py
@@ -1,3 +1,15 @@
+# TO DO
+# 1 stworzenie 2 serwisów, ktore pozwala odczytac:
+# a. aktualnie obsługiwany i (nazwa: getActualNumber)
+# b. pierwszy dostępny numerek z bazy (nazwa: getAvailableNumber)
+# 2 implementacjia serwisów w aplikacji mobilnej: getActualNumber; getAvailableNumber
+# 3 Logowanie studenta do ap mobilnej
+# 4 Logowanie employee do serwisu deanOffice
+# 5 REzerwacja numerka
+# a. mobilna
+# b. serwis rezerwujący (trivial - )
+
+
 from django.shortcuts import render
 from .models import Student
 from .models import Employee
@@ -33,6 +45,29 @@ def dean_next_number(request):
         num.delete()
 
     return dean_office(request)
+
+def dean_get_actual_number(request):
+
+    actual_number = NumbersQueue.objects.first()
+
+    if actual_number is not None:
+        num = actual_number.numberId
+    else:
+        num = 0
+
+    return render(request, 'dean_office/1.html', {'num': num})
+
+def dean_get_available_number(request):
+
+    available_number = NumbersQueue.objects.last()
+
+    if available_number is not None:
+        num = available_number.numberId + 1
+    else:
+        num = 1
+
+    return render(request, 'dean_office/1.html', {'num': num})
+
 
 def welfare_office(request):
     current_user = Employee.objects.first()
